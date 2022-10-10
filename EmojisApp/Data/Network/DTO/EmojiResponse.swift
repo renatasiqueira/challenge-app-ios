bbf6ccf8211a83888e18b9284b890c7716d0021b
Renata Siqueira <rsiqueira@blissapplications.com>
2022-10-10 12:04:19 +0100

    UIImage compile swift

---
+++ EmojisApp/Data/Network/DTO/EmojiResponse.swift
@@ -0,0 +1,8 @@
+//
+//  EmojiResponse.swift
+//  EmojisApp
+//
+//  Created by Renata Siqueira on 10/10/2022.
+//
+
+import Foundation
---
+++ EmojisApp/Utilities/ImageView+Download.swift
@@ -0,0 +1,8 @@
+//
+//  ImageView+Download.swift
+//  EmojisApp
+//
+//  Created by Renata Siqueira on 10/10/2022.
+//
+
+import Foundation
